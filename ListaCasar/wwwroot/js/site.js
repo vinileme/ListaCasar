720d11f21a3bcb838c7576cf4b299d67f5c4a666
vinileme <viniciuslcosta2013@gmail.com>
2022-06-02 15:10:11 -0300

    Incluindo barra de pesquisa e paginação na lista

--- ListaCasar/wwwroot/js/site.js
+++ ListaCasar/wwwroot/js/site.js
@@ -2,6 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#table-contatos').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
 
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
